2f1a31452cb59a1be08c053e1b357b76de8bebb4
Arandi Lopez <arandilopez.93@gmail.com>
2015-03-02 17:02:17 -0600

    New URL builder

---
+++ PVC/Core/URL.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+* URL class for build urls
+*/
+class URL {
+	
+	/**
+	 * Create an URL based on BASE_URL global
+	 * @param  string $route route in format as: controller.method[.params]
+	 * @return string        router in format as: http://example.com/controller/method[/params]
+	 */
+	public static function to($route=false)
+	{
+		if ($route) 
+		{
+			$url_path = str_replace(".", "/", $route);
+			return BASE_URL.$url_path;
+		}
+		else
+		{
+			return BASE_URL;
+		}
+	}
+}
